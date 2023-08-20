befc0fe0b4e7f218df461704730f4673b59390d5
unknown <vhae04.swem@example.com>
2023-08-20 19:29:57 +0700

    ok

--- test.py
+++ test.py
@@ -2,12 +2,15 @@ import os
 
 # Git commands
 commands = [
+    "git clone https://github.com/bugineverything/bugnote",
+    "cd bugnote",
+    "echo 1>new",
     "git init",
     "git remote add origin https://github.com/bugineverything/bugnote",
     "git branch -M main",
     "git add .",
     'git commit -m "ok"',
-    "git push -u origin main"
+    "git push -u origin main",
     "echo %cd%"
 ]
 
